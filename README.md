10e73e44e0793df3c6e885ea21c4b7cf11a91a03
GlenTsime <lehlohonologlen@gmail.com>
2020-05-20 22:03:53 +0200

    Code to create multiple Ubuntu VMS

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a repo for Terraform awesomeness
+* it focuses on Azure solutions managed / implemented using Terraform
+
+
+Author :  Glen Tsime (Dr Azure)
+email  :  GlenTsime@outlook.com
+
+
+Thank you!
---
+++ main.tf
@@ -0,0 +1,151 @@
+provider "azurerm" {
+  version = "2.0.0"
+  features  {}
+}
+
+variable "counts" {
+  default = 3
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-rg1"
+  location = "north europe"
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  address_space       = ["192.168.0.0/16"]
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-net1"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "192.168.1.0/24"
+}
+
+resource "azurerm_public_ip" "ipaddress" {
+  count = "${var.counts}"
+  name                = "terraformip${count.index}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Dynamic"
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  count = "${var.counts}"
+  name                = "terraformnic${count.index}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  security_rule {
+    
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  count = "${var.counts}"
+  name                = "terraformnic${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  ip_configuration {
+    name                          = "ipaddress${count.index}"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.ipaddress.*.id[count.index]}"
+    
+  }
+
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+#connecting the nic id to the security Id - association
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  count = "${var.counts}"
+  network_interface_id      = "${azurerm_network_interface.nic.*.id[count.index]}"
+  network_security_group_id = "${azurerm_network_security_group.nsg.*.id[count.index]}"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  count = "${var.counts}"
+  name                     = "terraform${count.index}store"
+  location                 = azurerm_resource_group.rg.location
+  account_replication_type = "LRS"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_kind             = "storage"
+  account_tier             = "standard"
+  tags = {
+    enviroment = "demo"
+  }
+}
+
+variable "var" {
+  default = "azuretest"
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count = "${var.counts}"
+  name                  = "terraformvm${count.index}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids =  ["${azurerm_network_interface.nic.*.id[count.index]}"]
+  size                  = "Standard_B1MS"
+
+  os_disk {
+    name                 = "server-${count.index}-os"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+
+  computer_name                   = "server-${count.index}-os"
+  admin_username                  = "schema"
+  disable_password_authentication = false
+  admin_password = "Morolong1993"
+  
+
+
+
+  tags = {
+    environment = "demo"
+  }
+}
+
